842c0f62303d459ae895e0e21371bfe184636cb1
Kevin Smith <keveightysev@gmail.com>
2019-04-24 18:24:20 -0500

    Refactored CSS reset into styled-component, started a Global style file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import CSSReset from './styles/CSSReset';
+import Global from './styles/Global';
+
 const App = () => (
   <>
+    <CSSReset />
+    <Global />
     <h1>Recaller</h1>
     <p> Test</p>
   </>
---
+++ src/styles/LandingPage.js
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+const LandingPage = styled.section``;
