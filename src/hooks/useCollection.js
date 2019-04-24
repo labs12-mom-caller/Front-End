b164eee0df63e425d5390898547fab0ea8469588
Michael <checomichael2@gmail.com>
2019-04-24 17:52:48 -0400

    custom hook for querying collections and adding filters

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
+
+export default function suseCollection(path, orderBy, where = []) {
+  const [docs, setDocs] = useState([]);
+  const [queryField, queryOperator, queryValue] = where;
+  useEffect(() => {
+    let collection = db.collection(path);
+    if (orderBy) {
+      collection = collection.orderBy(orderBy);
+    }
+    if (queryField) {
+      collection = collection.where(queryField, queryOperator, queryValue);
+    }
+    // .doc('general')
+    // .collection('messages')
+    // .limit(5)
+    return collection.onSnapshot(snapshot => {
+      const documents = [];
+      snapshot.forEach(doc => {
+        docs.push({
+          ...doc.data(),
+          id: doc.id,
+        });
+      });
+      setDocs(documents);
+    });
+  }, [path, orderBy, queryField, queryOperator, queryValue]);
+  return docs;
+}
