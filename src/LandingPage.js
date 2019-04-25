f0dfc3577f2717e19490dec82dd529219f234fbe
Michael <checomichael2@gmail.com>
2019-04-24 20:52:49 -0400

    handleSignIn function for google auth

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { firebase } from './firebase';
 
 const LandingPage = () => {
+  const [user, setUser] = React.useState(null);
+  const handleSignIn = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await firebase.auth().signInWithPopup(provider);
+    setUser(result.user);
+  };
   return <div>Recaller</div>;
 };
 
