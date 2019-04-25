f2d8a9ca8d1f5251a04c908363b66fc25c29af2f
shawn <shawnantonucci2018@gmail.com>
2019-04-24 23:01:53 -0700

    Restructured form to use hook and login to db

--- src/screens/SigninForm.js
+++ src/screens/SigninForm.js
@@ -1,66 +1,88 @@
 import React from 'react';
 import { Formik } from 'formik';
+import firebase from 'firebase';
 
-const SigninForm = () => (
-  <div>
-    <h1>Sign In With Email</h1>
+const SignupForm = () => {
+  const [user, setUser] = React.useState(null);
 
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          //   alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type='email'
-            name='email'
-            placeholder='Enter your email here'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type='password'
-            name='password'
-            placeholder='Enter your password here'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-          <button type='submit' disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+  const handleSignUp = async (email, password) => {
+    const provider = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        // Handle Errors here.
+        console.log(error, 'failed to signup');
+        // ...
+      });
+    const result = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    setUser(result.user);
+  };
 
-export default SigninForm;
+  return user ? (
+    <div>
+      <h2>You Are Logged In</h2>
+    </div>
+  ) : (
+    <div>
+      <h1>Signup with Email</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            // alert(JSON.stringify(values, null, 2));
+            handleSignUp(values.email, values.password);
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type='email'
+              name='email'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type='password'
+              name='password'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <button type='submit' disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default SignupForm;
