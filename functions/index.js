11400203ff4e9f3d6cfc4a9a7e0d6ae73cb5f5a9
Kevin Smith <keveightysev@gmail.com>
2019-04-25 13:50:38 -0500

    Started work on calling function, successfully getting data from contacts collection

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://recaller-14a1f.firebaseio.com',
+});
+
+const firestore = admin.firestore();
+
+exports.callService = functions.pubsub.topic('recaller').onPublish(() => {
+  const contacts = firestore.collection('contacts');
+  const today = contacts
+    .where('scheduled_day', '==', 'thursday')
+    .get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        console.log('No contacts');
+        return;
+      }
+      snapshot.forEach(doc => {
+        console.log(doc);
+      });
+    })
+    .catch(err => {
+      console.log('Error getting docs', err);
+    });
+
+  return 'ok';
+});
