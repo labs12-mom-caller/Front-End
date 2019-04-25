beaffc3a0d92e53a272ce767a9c0a0d29818f674
Kevin Smith <keveightysev@gmail.com>
2019-04-25 15:40:42 -0500

    Successfully getting phone number from user1

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const accountSid = functions.config().twilio.sid;
+const authToken = functions.config().twilio.token;
+const client = require('twilio')(accountSid, authToken);
+
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
@@ -10,23 +14,24 @@ admin.initializeApp({
 
 const firestore = admin.firestore();
 
-exports.callService = functions.pubsub.topic('recaller').onPublish(() => {
+exports.callService = functions.pubsub.topic('recaller').onPublish(async () => {
   const contacts = firestore.collection('contacts');
-  const today = contacts
-    .where('scheduled_day', '==', 'thursday')
-    .get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        console.log('No contacts');
-        return;
-      }
-      snapshot.forEach(doc => {
-        console.log(doc);
-      });
-    })
-    .catch(err => {
-      console.log('Error getting docs', err);
+  const users = firestore.collection('users');
+  try {
+    const snapshot = await contacts
+      .where('scheduled_day', '==', 'thursday')
+      .get();
+    if (snapshot.empty) {
+      console.log('No contacts');
+      return;
+    }
+    snapshot.forEach(async doc => {
+      const user1id = doc.data().user1.id;
+      const user1 = await users.doc(user1id).get();
+      const user1phone = user1.data().phone;
+      console.log(doc.id, user1phone);
     });
-
-  return 'ok';
+  } catch (err) {
+    console.log('Error getting docs', err);
+  }
 });
