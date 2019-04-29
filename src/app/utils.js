7f4dd2bfad2237d21a693fdd025a573c45c53086
Michael <checomichael2@gmail.com>
2019-04-29 09:50:11 -0400

    remove commented function

--- src/app/utils.js
+++ src/app/utils.js
@@ -4,29 +4,7 @@ import { firebase, db, auth } from '../firebase';
 export function logout() {
   return auth().signOut();
 }
-// const handleSubmit = async values => {
-//   console.log(values);
-//   const formattedPhone = String('+1').concat(
-//     String(values.phoneNumber).replace(/[^\d]/g, ''),
-//   );
-//   try {
-//     const { user } = await firebase
-//       .auth()
-//       .createUserWithEmailAndPassword(values.email, values.password);
-//     const ref = db.doc(`users/${user.uid}`);
-//     const { email, photoURL } = user;
-//     ref.set({
-//       displayName: values.displayName,
-//       photoURL,
-//       email: values.email,
-//       phoneNumber: formattedPhone,
-//     });
-//     console.log(user, '$$$$$$');
-//     user.updateProfile({ displayName: values.displayName });
-//   } catch (error) {
-//     alert(error);
-//   }
-// };
+
 export async function signup({
   email,
   password,
