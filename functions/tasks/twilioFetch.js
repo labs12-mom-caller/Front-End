7b92da7d79bf71911f25dee87b48115bd195b1bd
Kevin Smith <keveightysev@gmail.com>
2019-04-30 07:13:31 -0500

    Successfully downloading file from Twilio and storing in Cloud Storage

--- functions/tasks/twilioFetch.js
+++ functions/tasks/twilioFetch.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
-const fs = require('fs');
+const request = require('request');
 
 const accountSid = functions.config().twilio.sid;
 const authToken = functions.config().twilio.token;
@@ -17,15 +17,32 @@ exports.handler = async (req, res, firestore, storage) => {
     snapshot.forEach(async doc => {
       const { twilio } = doc.data();
       const [recording] = await client.recordings.list({ sourceSid: twilio });
-      const { sid } = recording;
-      const { id } = doc;
-      const file = storage.file(`${id}.wav`);
-      fs.createReadStream(
-        `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Recordings/${sid}`,
-      )
-        .pipe(file.createWriteStream())
-        .on('error', err => console.log(err))
-        .on('finish', () => {});
+      const { sid, status } = recording;
+      if (status === 'completed') {
+        const { id } = doc;
+        const file = storage.file(`${id}`);
+        request(
+          `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Recordings/${sid}.wav`,
+        )
+          .pipe(
+            file.createWriteStream({
+              metadata: { contentType: 'audio/wav' },
+            }),
+          )
+          .on('error', err => console.log(err))
+          .on('finish', async () => {
+            const url = await file.getSignedUrl({
+              action: 'read',
+              expires: '01-01-3000',
+            });
+            doc.set({
+              audio: url,
+              fetched: true,
+            });
+          });
+      } else {
+        console.log('Call has not finished recording');
+      }
     });
   } catch (err) {
     console.log(err);
