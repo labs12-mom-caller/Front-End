d7855fe421368b08c3f5f57cdad4a69481d5241d
Kevin Smith <keveightysev@gmail.com>
2019-05-05 20:09:46 -0500

    Disabled linter for audio tag

--- src/components/dashboard/PreviousCalls.js
+++ src/components/dashboard/PreviousCalls.js
@@ -58,6 +58,7 @@ const PreviousCalls = ({ userId }) => {
             <p>{moment(call.call_time).format('dddd, MMMM Do [at] h:mm A')}</p>
             <p>Call duration: {call.call_duration} seconds</p>
             <p>Listen to call</p>
+            {/* eslint-disable jsx-a11y/media-has-caption */}
             <audio controls>
               <source src={call.audio} type='audio/wav' />
               <track />
