47cd7d47403c8a455183980957fa4cc445167a85
shawn <shawnantonucci2018@gmail.com>
2019-05-06 11:20:16 -0700

    working on ScheduleContacts

--- src/components/dashboard/ScheduledContacts.js
+++ src/components/dashboard/ScheduledContacts.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment-timezone';
+import { db } from '../../firebase';
 
-const ScheduledContacts = () => {
+const ScheduledContacts = ({ user }) => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const { uid } = user;
+    // console.log(uid, 'from contacts');
+
+    const fetchData = async () => {
+      const user = await db.collection('users').doc(uid);
+      const userContacts = await db
+        .collection('contacts')
+        .where('user1', '==', user)
+        .get();
+      // console.log(userContacts.docs[0].id, 'from useEffect');
+
+      await userContacts.forEach(async doc => {
+        const allContacts = await db
+          .collection('contacts')
+          .where(`${userContacts.docs.id}`, '==', `${user.uid}`)
+          .get();
+
+          console.log(userContacts, "from contacts")
+
+        if (!allContacts.empty) {
+          allContacts.forEach(async doc => {
+            const contactData = {
+              // callId: doc.id,
+              // user2: {},
+              // contactId: '',
+              // audio: doc.data().audio,
+              // call_duration: doc.data().call_duration,
+              // call_time: moment(doc.data().call_time.toDate()).format(),
+              user2: {},
+              next_call: moment(doc.data().call_time.toDate()).format(),
+
+            };
+            const contactRef = doc.data().userContacts.docs.id.path;
+            await db.doc(contactRef).onSnapshot(async doc => {
+              contactData.user2 = doc.data().user2;
+              await db.doc(doc.data().user2.path).onSnapshot(doc => {
+                contactData.user2 = {
+                  displayName: doc.data().displayName
+                };
+                setContacts(c => [...c, contactData]);
+              });
+            });
+          });
+        }
+      });
+    };
+    fetchData();
+  }, [user]);
+
   return (
     <>
-      <h2>Review Your Currently Scheduled Contacts</h2>
+      <h2>List of your contacts</h2>
+      {console.log(contacts, 'from return')}
+      {contacts &&
+        contacts.map(contact => (
+          <div key={contact.user1}>
+            {/* <h3>Call with {contact.user2.displayName}</h3> */}
+            <h3>At {contact.next_call}</h3>
+          </div>
+        ))}
     </>
   );
 };
