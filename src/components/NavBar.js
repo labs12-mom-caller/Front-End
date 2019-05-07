d2e1c4e8095c3e7e8bb7b435d3487b5ff786fda7
Michael <checomichael2@gmail.com>
2019-05-07 15:38:54 -0400

    updated navbar to match design and removed extra links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ import { Link, navigate } from '@reach/router';
 import { logout } from '../app/utils';
 
 const NavbarPage = ({ user }) => {
+  console.log(user, 'ello!');
   const [collapseId, setCollapseId] = useState('');
 
   const toggleCollapse = newId => () => {
@@ -21,92 +22,66 @@ const NavbarPage = ({ user }) => {
   };
 
   return (
-    <div>
-      <Navbar className={collapseId ? 'Navbar__ToggleShow' : null}>
-        <div className='NavbarLink NavbarLink-brand'>ReCaller</div>
-        <nav
-          className={
-            collapseId
-              ? 'Navbar__Items Navbar__ToggleShow'
-              : 'Navbar__Items Navbar__Items--right'
-          }
-        >
-          <li
-            className='NavbarLink'
-            style={{ color: '#6B6D76', listStyle: 'none' }}
-          >
-            <Link style={{ color: '#083D77', padding: '5px' }} to='/'>
-              Dashboard
-            </Link>
-          </li>
-          <li
-            className='NavbarLink'
-            style={{ color: '#6B6D76', listStyle: 'none' }}
-          >
-            <Link
-              style={{ color: '#083D77', padding: '5px' }}
-              to={`/choose/${user.uid}`}
-            >
-              Add New Call
-            </Link>
-          </li>
-          <li
-            className='NavbarLink'
-            style={{ color: '#6B6D76', listStyle: 'none' }}
-          >
-            <Link style={{ color: '#083D77', padding: '5px' }} to='/'>
-              Currently Scheduled Calls
-            </Link>
-          </li>
-          <li
-            className='NavbarLink'
-            style={{ color: '#6B6D76', listStyle: 'none' }}
-          >
-            <Link
-              style={{ color: '#083D77', padding: '5px' }}
-              to={`/prev-calls/${user.uid}`}
-            >
-              Previous Calls
-            </Link>
-          </li>
-          <li
-            className='NavbarLink'
-            style={{ color: '#6B6D76', listStyle: 'none' }}
-          >
-            {/* <Link
-                style={{ color: '#083D77', padding: '5px' }}
-                to={`/choose/${user.uid}`}
-              >
-                Choose Contact
-              </Link> */}
-          </li>
-          <li
-            className='NavbarLink'
-            style={{ color: '#6B6D76', listStyle: 'none' }}
-          >
-            <Link
-              style={{ color: '#083D77', padding: '5px' }}
-              to='/'
-              onClick={logoutHandler}
-            >
-              Sign Out
-            </Link>
-          </li>
-        </nav>
-        <div
-          onClick={toggleCollapse('navbarCollapse1')}
-          id='navbarCollapse1'
-          isopen={collapseId}
-          className='NavbarLink NavbarLink-toggle'
-        >
-          <i className='fas fa-bars' />
-        </div>
-        <Hr className='hr-underline' />
-      </Navbar>
-    </div>
+    <Container>
+      <Name>
+        <Link to='/'>ReCaller</Link>
+      </Name>
+      <Nav>
+        <li>
+          <Link to={`/account/${user.uid}`}>Account</Link>
+        </li>
+        <li>
+          <VL />
+        </li>
+        <li>
+          <Link to='/' onClick={logoutHandler}>
+            Sign Out
+          </Link>
+        </li>
+      </Nav>
+    </Container>
   );
 };
-
+const Name = styled.h1`
+  grid-column: 1 / span 1;
+  margin-top: 7px;
+  margin-left: 13px;
+  padding: 5px;
+  a {
+    color: #083d77;
+    font-family: 'Pacifico';
+    font-size: 42px;
+  }
+`;
+const VL = styled.div`
+  border-left: 1px solid #999999;
+  height: 25px;
+  position: absolute;
+  right: 4.8%;
+  margin-left: -3px;
+  top: 2.4%;
+`;
+const Container = styled.div`
+  border: 0.5px solid #000000;
+  display: grid;
+  grid-column: 1 / -1;
+`;
+const Nav = styled.nav`
+  display: flex;
+  width: 100%;
+  grid-column: 3 / span 2;
+  justify-content: flex-end;
+  align-items: center;
+  list-style: none;
+  a {
+    color: #999999;
+    font-weight: 550;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    margin: 0 5px;
+    padding: 3px;
+  }
+`;
 NavbarPage.propTypes = {
   user: PropTypes.shape({
     displayName: PropTypes.string,
@@ -118,74 +93,3 @@ NavbarPage.propTypes = {
 };
 
 export default NavbarPage;
-
-const Navbar = styled.div`
-  background-color: transparent;
-  display: flex;
-  padding: 16px;
-  justify-content: space-between;
-  font-family: sans-serif;
-  color: white;
-  max-width: 100%;
-  .NavbarLink {
-    padding-right: 8px;
-    font-size: 100%;
-    max-width: 100%;
-    white-space: nowrap;
-  }
-  .NavbarLink-brand {
-    color: #083d77;
-    font-size: 3em;
-    font-family: pacifico;
-    margin-left: 2.5%;
-    @media only screen and (max-width: 768px) {
-      font-size: 2.5rem;
-      align-self: center;
-    }
-  }
-  .Navbar__Items {
-    display: flex;
-  }
-  .Navbar__Items--right {
-    margin-left: auto;
-    align-self: center;
-  }
-  .NavbarLink-toggle {
-    display: none;
-  }
-  @media only screen and (max-width: 768px) {
-    .Navbar__Items,
-    .Navbar {
-      flex-direction: column;
-    }
-    .Navbar__Items {
-      display: none;
-    }
-    .Navbar__Items--right {
-      margin-left: 0;
-    }
-    .Navbar__ToggleShow {
-      display: flex;
-    }
-    .NavbarLink-toggle {
-      align-self: flex-end;
-      color: #083d77;
-      display: initial;
-      align-self: center;
-      font-size: 2.5rem;
-      cursor: pointer;
-    }
-  }
-`;
-
-const Hr = styled.hr`
-  border-color: #083d77;
-  width: 70%;
-  margin-left: 30%;
-  position: absolute;
-  top: 75px;
-
-  @media only screen and (max-width: 768px) {
-    display: none;
-  }
-`;
