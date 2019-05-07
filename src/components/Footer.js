a96cff5c0c375d436a0e6f2e47f6fbc5d76c2a3c
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-07 16:58:35 -0400

    Styled Footer Based on Figma Design. social icons on hover highlight to hex code of the company

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { styles } from 'ansi-colors';
 
 const Footer = () => {
   return (
     <FooterWrap>
       <SectionLeft className='sectionLeft' style={{}}>
-        <p style={{ color: 'white' }}>
-          We believe in connecting friends and family by offering scheduled
-          calls and call transcription. Always remember the things you’ve talked
-          about with your loved ones. We are ReCaller.
-        </p>
+        <p style={{ color: 'white' }}>ReCaller</p>
+        <div>
+          <FaFacebook className='icon facebook' />
+          <FaLinkedin className='icon linkedin' />
+          <FaTwitter className='icon twitter' />
+        </div>
         <span style={{ color: 'white' }}>
           &copy; {new Date().getFullYear()}
           <a href='/' style={{ color: 'white' }}>
@@ -20,30 +23,20 @@ const Footer = () => {
       </SectionLeft>
       <SectionRight>
         <ul>
-          <li className='list' style={{ color: '#6B6D76' }}>
-            <a style={{ color: '#FF6F61' }} href='/'>
-              Dashboard
-            </a>
+          <li className='list'>
+            <a href='/'>Dashboard</a>
           </li>
-          <li className='list' style={{ color: '#6B6D76' }}>
-            <a style={{ color: '#FF6F61' }} href='#!'>
-              Add New Call
-            </a>
+          <li className='list'>
+            <a href='#!'>Add New Call</a>
           </li>
-          <li className='list' style={{ color: '#6B6D76' }}>
-            <a style={{ color: '#FF6F61' }} href='#!'>
-              Review Calls
-            </a>
+          <li className='list'>
+            <a href='#!'>Review Calls</a>
           </li>
-          <li className='list' style={{ color: '#6B6D76' }}>
-            <a style={{ color: '#FF6F61' }} href='#!'>
-              Previous Calls
-            </a>
+          <li className='list'>
+            <a href='#!'>Previous Calls</a>
           </li>
-          <li className='list' style={{ color: '#6B6D76' }}>
-            <a style={{ color: '#FF6F61' }} href='/about-us'>
-              Our Team
-            </a>
+          <li className='list'>
+            <a href='/about-us'>Our Team</a>
           </li>
         </ul>
       </SectionRight>
@@ -53,46 +46,95 @@ const Footer = () => {
 
 export default Footer;
 
-const FooterWrap = styled.div`
+const FooterWrap = styled.footer`
   display: flex;
-  background-color: #083d77;
-  /* height: 250px; */
-  height: 17%;
+  justify-content: space-between;
+  background-color: #636578;
+  height: 15vh;
+  width: 100%;
   @media (max-width: 992px) {
-    margin-top: 40%;
+    /* margin-left: 6%; */
   }
 `;
 const SectionLeft = styled.div`
   width: 60%;
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
-  padding-left: 2%;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-left: 3%;
   width: 48%;
-  @media (max-width: 992px) {
-    p {
-      padding: 5px;
-      font-size: 12px;
+  @media (min-width: 992px) {
+    margin-left: 5%;
+  }
+  div {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    /* border: 1px solid red; */
+    width: 50%;
+    margin-left: -6%;
+    @media (min-width: 992px) {
+      margin-left: -7%;
     }
-    span {
-      text-align: center;
-      margin-left: 5%;
+    .icon {
+      font-size: 2rem;
+      transition: all 1s ease-in-out;
+    }
+    .facebook {
+      &:hover {
+        color: #3b5998;
+      }
+    }
+    .twitter {
+      &:hover {
+        color: #1da1f2;
+      }
+    }
+    .linkedin {
+      &:hover {
+        color: #0077b5;
+      }
+    }
+  }
+  p {
+    font-size: 1rem;
+    margin-top: 5%;
+    @media (min-width: 992px) {
+      font-size: 1.5rem;
+      margin-top: 1%;
+    }
+  }
+  span {
+    margin-bottom: 5%;
+    @media (min-width: 992px) {
+      font-size: 1.5rem;
+      margin-bottom: 1%;
     }
   }
 `;
 const SectionRight = styled.div`
-  /* align-self: flex-end; */
-  margin-left: 4%;
-  margin-top: 2.6%;
-  width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-right: 5%;
   font-weight: bold;
-  font-size: 18px;
   line-height: 1.5;
-  @media (max-width: 992px) {
-    padding: 5px;
-    margin-top: 8%;
-    margin-right: 2%;
-    font-size: 15px;
-    line-height: 1.5;
+  @media (min-width: 992px) {
+    margin-right: 7%;
+  }
+  ul {
+    li {
+      list-style: none;
+      color: white;
+      @media (min-width: 992px) {
+        font-size: 1.2rem;
+      }
+    }
+  }
+  a {
+    color: white;
   }
 `;
