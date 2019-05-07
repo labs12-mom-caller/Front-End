a8eafb615074c119daccc4a2635a8c6f42215114
Kevin Smith <keveightysev@gmail.com>
2019-05-07 09:43:48 -0500

    Basic ContactInfo.js page complete

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function App() {
         <CallRecord path='prev-calls/:userId/:callId' />
         <AboutUs path='/about-us' />
         <UpdateAccount user={user} path='/account/:userId' />
-        <ContactInfo path='/contact/:contactId/' />
+        <ContactInfo user={user} path='/contact/:contactId/' />
       </Router>
       <Footer />
     </>
---
+++ src/styles/ContactInfoPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ContactInfoPage = styled.section`
+  .contact-header {
+    display: flex;
+
+    img {
+      width: 25%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+`;
