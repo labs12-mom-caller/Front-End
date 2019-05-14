93cd648ac469cb011d8b1b55aadf9cdb6a328bab
keveightysev <keveightysev@gmail.com>
2019-05-14 10:11:32 -0500

    Working on refactoring twilio fetch

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const stripe = require('stripe')(functions.config().stripe.token);
-const express = require('express');
-const cors = require('cors')({ origin: true });
 const sgMail = require('@sendgrid/mail');
+
 const caller = require('./tasks/caller.js');
 const fetch = require('./tasks/twilioFetch.js');
 const contactEmail = require('./tasks/contactEmail.js');
-
-const app = express();
 const serviceAccount = require('./serviceAccountKey.json');
 
 sgMail.setApiKey(functions.config().sendgrid.key);
@@ -27,61 +23,6 @@ exports.callService = functions.pubsub
   .onPublish((req, res) => {
     return caller.handler(req, res, firestore);
   });
-
-exports.twilioFetch = functions.pubsub
-  .topic('twilioFetch')
-  .onPublish((req, res) => {
-    return fetch.handler(req, res, firestore, bucket);
-  });
-function send(res, code, body) {
-  res.send({
-    statusCode: code,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(body),
-  });
-}
-function charge(req, res) {
-  const body = JSON.parse(req.body);
-  const token = body.token.id;
-  const { amount } = body.charge;
-  const { currency } = body.charge;
-
-  // Charge card
-  stripe.charges
-    .create({
-      amount,
-      currency,
-      description: 'Firebase Example',
-      source: token,
-    })
-    .then(charge => {
-      send(res, 200, {
-        message: 'Success',
-        charge,
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      send(res, 500, {
-        error: err.message,
-      });
-    });
-}
-
-app.use(cors);
-app.post('/', (req, res) => {
-  // Catch any unexpected errors to prevent crashing
-  try {
-    charge(req, res);
-  } catch (e) {
-    console.log(e);
-    send(res, 500, {
-      error: `The server received an unexpected error. Please try again and contact the site admin if the error persists.`,
-    });
-  }
-});
-
-exports.charge = functions.https.onRequest(app);
 
 exports.signupUserTwo = functions.firestore
   .document(`/users/{useruid}`)
@@ -140,3 +81,8 @@ exports.contactEmail = functions.firestore
   .onCreate((snapshot, context) => {
     return contactEmail.handler(snapshot, context, firestore);
   });
+
+exports.twilio = functions.https.onRequest((req, res) => {
+  fetch.handler(req, res, firestore, bucket);
+  res.status(201).end();
+});
