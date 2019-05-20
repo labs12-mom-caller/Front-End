38457d88819e4b937895acfeca18c71e7d02b821
Michael <checomichael2@gmail.com>
2019-05-20 11:54:21 -0400

    testing email input change

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -1,7 +1,7 @@
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import Login from '../components/Auth/Login';
 
 test('should render inputs properly ', () => {
@@ -10,4 +10,6 @@ test('should render inputs properly ', () => {
   const password = getByLabelText(/password/i);
   const SignInButton = getByTestId(/button/i);
   debug(SignInButton);
+  fireEvent.change(email, { target: { value: 'checomichael2@gmail.com' } });
+  expect(email.value).toBe('checomichael2@gmail.com');
 });
