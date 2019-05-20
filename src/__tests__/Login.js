1e05354666c0e04e2454040414cb76c869500e84
Michael <checomichael2@gmail.com>
2019-05-20 12:23:15 -0400

    basic test for login form

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -1,15 +1,17 @@
 import 'jest-dom/extend-expect';
-import 'react-testing-library/cleanup-after-each';
 import React from 'react';
+import { navigate } from '@reach/router';
 import { render, fireEvent } from 'react-testing-library';
 import Login from '../components/Auth/Login';
 
 test('should render inputs properly ', () => {
-  const { getByLabelText, debug, getByTestId, getByText } = render(<Login />);
+  const { getByLabelText, debug, getByTestId, rerender } = render(<Login />);
   const email = getByTestId(/email/i);
   const password = getByLabelText(/password/i);
   const SignInButton = getByTestId(/button/i);
-  debug(SignInButton);
+  expect(email.value).toBe('');
   fireEvent.change(email, { target: { value: 'checomichael2@gmail.com' } });
   expect(email.value).toBe('checomichael2@gmail.com');
+  rerender(<Login />);
+  expect(window.localStorage.getItem('email')).toBeTruthy();
 });
